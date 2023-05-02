83f4bf0b4b035d165ca8ad9b2a40d08f0e9579f2
Sosa Osvaldo <129412315+SosaOsv09@users.noreply.github.com>
2023-05-02 15:14:22 -0300

    js new

--- js/codigo.js
+++ js/codigo.js
@@ -1,1 +1,13 @@
 alert ("Bievenidos a la página de TransporteX")
+
+console.log ("Este texto sirve de ejemplo")
+
+const nombre = "Osvaldo";
+const apellido = "Sosa";
+const edad = 26;
+const hobbies = ["Leer", "Videojuegos", "Series/Peliculas"];
+
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
+console.log(hobbies);
